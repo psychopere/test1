fdd15c4eb561a0249c21efd614850ea97588cb2b
psychopere <127682782+psychopere@users.noreply.github.com>
2023-11-24 20:40:11 +0530

    error in adding JS

--- script.js
+++ script.js
@@ -32,3 +32,5 @@ document.getElementById("button2").addEventListener("click", function () {
 
   console.log("Button 2 clicked. Model URL: " + assetEntity.getAttribute("gltf-model"));
 });
+
+console.log(pakaya)
