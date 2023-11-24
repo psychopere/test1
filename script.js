33ac41f2501507c300bf053ee12b1bae6d8362b1
psychopere <127682782+psychopere@users.noreply.github.com>
2023-11-24 19:15:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ var url02 = "assests/light_aircraft/scene.gltf";
 var url03 = "assests/etrich_taube/scene.gltf";
 
 document.getElementById("button1").addEventListener("click", function () {
-    for (var i = 0; i < 2; i++) {
-        url01 = url02;
-        url02 = url03;
-        url03 = url01;
-      }
+  url01 = url02;
+  url02 = url03;
+  url03 = url01;
   // Change the asset when the button is clicked
   var assetEntity = document.getElementById("model_01");
   assetEntity.setAttribute("gltf-model", url01);
 });
-
