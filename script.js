ec4e7f9f509262447fc4ce8f2a691a290ca8b9c2
psychopere <127682782+psychopere@users.noreply.github.com>
2023-11-24 20:12:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 var url01 = "assests/aeroplane/scene.gltf";
 var url02 = "assests/light_aircraft/scene.gltf";
 var url03 = "assests/etrich_taube/scene.gltf";
+var i = 1;
 
 document.getElementById("button1").addEventListener("click", function () {
-    var temp = url01;
-    url01 = url02;
-    url02 = url03;
-    url03 = temp
-  // Change the asset when the button is clicked
-  var assetEntity = document.getElementById("model_01");
-  assetEntity.setAttribute("gltf-model", url02);
-});
-
-document.getElementById("button2").addEventListener("click", function () {
-    var temp = url01;
-    url01 = url02;
-    url02 = url03;
-    url03 = temp
   // Change the asset when the button is clicked
   var assetEntity = document.getElementById("model_01");
   assetEntity.setAttribute("gltf-model", url01);
+  ispreviousbuttonclicked = true;
 });
 
+document.getElementById("button2").addEventListener("click", function () {
+  // Change the asset when the button is clicked
+  var assetEntity = document.getElementById("model_01");
+  assetEntity.setAttribute("gltf-model", url03);
+  isnextbuttonclicked = true;
+});
 
+if (ispreviousbuttonclicked) {
+  var temp = url03;
+  url03 = url02;
+  url02 = url01;
+  url01 = temp;
+} else if (isnextbuttonclicked) {
+  var temp = url01;
+  url01 = url02;
+  url02 = url03;
+  url03 = temp;
+}
