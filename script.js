560bc09cb5f63954c3bf93d5677d84a9379695db
psychopere <127682782+psychopere@users.noreply.github.com>
2023-11-24 19:41:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ document.getElementById("button1").addEventListener("click", function () {
     url03 = temp
   // Change the asset when the button is clicked
   var assetEntity = document.getElementById("model_01");
+  assetEntity.setAttribute("gltf-model", url02);
+});
+
+document.getElementById("button2").addEventListener("click", function () {
+    var temp = url01;
+    url01 = url02;
+    url02 = url03;
+    url03 = temp
+  // Change the asset when the button is clicked
+  var assetEntity = document.getElementById("model_01");
   assetEntity.setAttribute("gltf-model", url01);
 });
+
+
